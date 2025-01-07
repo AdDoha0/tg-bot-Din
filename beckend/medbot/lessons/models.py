3ce3ee77fcc8980980fb3300473cc027bd1a6289
Adam <adamdoxaev5@gmail.com>
2025-01-07 22:40:31 +0300

    wrote models and registered them

--- beckend/medbot/lessons/models.py
+++ beckend/medbot/lessons/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Volume(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=200)
+    summary = models.TextField()
+    created_ar = models.DateTimeField(auto_now_add=True)
+    volume = models.ForeignKey(Volume, on_delete=models.CASCADE, related_name='lessons')  # Связь с томом
+    video_url = models.URLField(blank=True, null=True)  # Ссылка на видео
+
+    def __str__(self):
+        return self.title
+
+
+class Words(models.Model):
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, related_name='words')  # Связь с уроком
+    term = models.CharField(max_length=100)  # Само слово
+    definition = models.TextField()  # Определение или перевод слова
+
+
+
+
