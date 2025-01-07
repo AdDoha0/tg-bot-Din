520aca6acbc5652be5f9c8f826cebce81f7e904b
Adam <adamdoxaev5@gmail.com>
2025-01-07 22:41:40 +0300

    wrote models and registered them

--- beckend/medbot/lessons/admin.py
+++ beckend/medbot/lessons/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+
+@admin.register(Volume)
+class VolumeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
+    search_fields = ('title', 'description')
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('title', 'volume', 'created_ar', 'video_url')
+    list_filter = ('volume', 'created_ar')
+    search_fields = ('title', 'summary')
+
+@admin.register(Words)
+class WordsAdmin(admin.ModelAdmin):
+    list_display = ('term', 'lesson', 'definition')
+    list_filter = ('lesson',)
+    search_fields = ('term', 'definition')
--- beckend/medbot/medbot/settings.py
+++ beckend/medbot/medbot/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',  # DRF
-    
-    'lessons',   
+
+    'lessons',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'UTC'
 
